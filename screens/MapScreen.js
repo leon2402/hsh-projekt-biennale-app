e4dca3c3cfdf5b1d35a8a2d521feccb3be2e5c9d
ksbim <krishna.schulze@stud.hs-hannover.de>
2019-03-26 12:49:28 +0100

    MapScreen mapview

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MapView } from 'expo';
 import { Alert, StyleSheet } from 'react-native';
 import { Container, Header, Title, Button, Left, Right, Body, Icon, Content, Text } from 'native-base';
 
@@ -6,6 +7,10 @@ export default class MapScreen extends React.Component {
   static navigationOptions = {
     drawerLabel: 'Map '
   };
+state={
+    longitude: 10.327924,
+    latitude: 45.435768,
+}
   render() {
     return (
       <Container>
@@ -20,21 +25,24 @@ export default class MapScreen extends React.Component {
           </Body>
           <Right />
         </Header>
-        <Content>
-          <Button onPress={() => { Alert.alert('You tapped the button!');}} success >
-            <Text>Klick mich!</Text>
-          </Button>
-        </Content>
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 45.435768,
+          longitude: 12.327924,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        >
+        <MapView.Marker
+        coordinate={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude}}
+        />
+        </MapView>
+
       </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
